3340bccd3959382bb26f46ac5769ce5023b2f456
Hana Wubet <hannnawubet12@yahoo.com>
2022-07-26 02:37:49 +0300

    netflix

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "2272275a8f240e69fe86657eeb263ac6";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+};
+export default requests;
